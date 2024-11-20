91fef8138dd393d30c6da5de6d357fc5d01e1e01
John Paul Jayme <jpaul.jayme@gmail.com>
2024-11-20 19:56:06 +0800

    WIP : Added Hero, Projects.

---
+++ src/App.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+  import book from "./assets/my_book.png";
+</script>
+
+<div class="wrapper">
+  <div class="menu">
+    <span class="menu__text">Menu</span>
+  </div>
+  <header class="header">
+    <h1 class="header__title col">
+      <span>The pages of</span>
+      <br>
+      <span>John Paul Jayme</span>
+    </h1>
+    <ul class="header__links">
+      <li>Web</li>
+      <li>Design</li>
+      <li>Development</li>
+      <li>Literature</li>
+      <li>Reviews</li>
+      <li>Organic</li>
+      <li>Volunteering</li>
+      <li>Cats and Dogs</li>
+    </ul>
+    <div class="header__hero">
+      <img class="hero__book" alt="My artwork" src={book} />
+    </div>
+  </header>
+  <section class="about">
+    <h2>About</h2>
+    <h3>
+      Creating intentional web experience for myself and the unlikely, curious
+      world
+    </h3>
+  </section>
+  <section class="projects">
+    <h2>CEBOOKLUB</h2>
+    <h2>THE FIG TREE</h2>
+    <h2>JAZEL'S CATTITUDES</h2>
+  </section>
+</div>
+
+<style>
+  .wrapper {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, auto);
+    gap: var(--space-m);
+  }
+
+  .header {
+    display: grid;
+    grid-template-rows: min-content auto;
+    min-height: 100vh;
+    gap: var(--space-m);
+    padding: var(--space-s);
+  }
+
+  .header__title {
+    /* display: flex;
+    flex-direction: column; */
+    font-size: 1rem;
+
+  }
+
+  .menu {
+    position: fixed;
+    top: var(--space-s);
+    right: var(--space-s);
+    cursor: pointer;
+    z-index: 10;
+    /* padding: var(--space-s); */
+  }
+
+  .menu__text {
+    writing-mode: vertical-rl;
+    transform: rotate(180deg);
+  }
+
+  .header__hero{
+    padding: var(--space-m);
+  }
+
+  .hero__book{
+    animation: rotate 15s linear infinite;
+    -moz-animation: rotate 15s linear infinite;
+    transform-style: preserve-3d;
+  }
+
+  @keyframes rotate {
+    from {
+        transform: rotateY(0deg);
+    }
+    to {
+        transform: rotateY(360deg);
+    }
+  }
+
+  .about{
+      display: grid;
+      grid-template-columns: minmax(0, 1fr) minmax(0, 2.16fr);
+      gap: var(--space-m);
+    }
+
+  .projects{
+    display: grid;
+    grid-template-columns: 
+      repeat(auto-fill, minmax(150px, 1fr));
+    gap: var(--space-m);
+    font-weight: bolder;
+  }
+</style>
