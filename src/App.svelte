4f8bade9cd3bc971534186bc017fcdf49d497a05
John Paul Jayme <jpaul.jayme@gmail.com>
2024-11-29 23:36:12 +0800

    Fixed routes on nav

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,10 @@
   import Layout from "./Layout.svelte";
   import { setupRoutes } from "./routes.svelte.js";
   import Home from './routes/HomeRoute.svelte'
-
-  // Define reactive state for the current component
+  
   let currentComponent = $state(Home);
 
-  // Set up routes and pass a function to update the state
   setupRoutes((component) => (currentComponent = component));
 </script>
 
-<!-- Pass the reactive currentComponent state to the Layout -->
 <Layout {currentComponent} />
